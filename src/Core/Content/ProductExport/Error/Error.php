bc101016ce978d916b9f694fe495af81847010a1
Michael Telgmann <m.telgmann@shopware.com>
2026-03-27 14:07:57 +0100

    fix double square brackets in array annotations

--- src/Core/Content/ProductExport/Error/Error.php
+++ src/Core/Content/ProductExport/Error/Error.php
@@ -25,7 +25,7 @@ abstract class Error extends \Exception implements \JsonSerializable, AssignArra
     abstract public function getParameters(): array;
 
     /**
-     * @return array<string, mixed>
+     * @return array<mixed>
      */
     abstract public function getErrorMessages(): array;
 
